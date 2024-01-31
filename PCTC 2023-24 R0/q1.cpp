b2aad520a5fee3748fde38b186b00887928d6cf3
FundayStrike <bryan_tan_boon_jun@students.edu.sg>
2024-01-31 17:25:27 +0800

    Add files via upload

---
+++ PCTC 2023-24 R0/q1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false); cin.tie(0);
+  int x, y, z;
+  cin >> x;
+  cin >> y;
+  cin >> z;
+  cout << x + (y*z)*12;
+}
